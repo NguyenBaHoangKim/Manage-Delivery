91e228fc0284caf40cb0aee571729875e151ece5
NguyenBaHoangKim <baloctq@gmail.com>
2023-12-12 22:15:55 +0700

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './components/demo/demo.component';
+import { NavigatorComponent } from './components/navigator/navigator.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
+    DemoComponent,
+    NavigatorComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
