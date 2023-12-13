e0f34338818d77646babff8a7733f1c297a0c762
Quan Kien <qtkk20a@gmail.com>
2023-12-13 18:49:52 +0700

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { DemoComponent } from './components/demo/demo.component';
 import { NavigatorComponent } from './components/navigator/navigator.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SearchingComponent } from './components/searching/searching.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DemoComponent,
     NavigatorComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    SearchingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/searching/searching.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-searching',
+  templateUrl: './searching.component.html',
+  styleUrl: './searching.component.scss'
+})
+export class SearchingComponent {
+
+}
