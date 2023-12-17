d7a5c3612c937478779c9d1dc607186b257daa07
Quan Kien <qtkk20a@gmail.com>
2023-12-17 23:19:13 +0700

    sunday

--- src/app/components/oderaccept/oderaccept.component.ts
+++ src/app/components/oderaccept/oderaccept.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './oderaccept.component.scss'
 })
 export class OderacceptComponent {
+accept = { id: 'VD001', type: 'giấy tờ', status: 'đang đến' }; // Thông tin vận đơn
 
+// {
+//   orderId: 'XYZ123',
+//   orderName: 'ABC',
+//   time: '12:00 PM, 25/12/2023',
+//   location: 'HCMC, Vietnam',
+//   status: 'Đang giao hàng'
+// },
+// Thêm các đơn hàng khác vào đây
 }
