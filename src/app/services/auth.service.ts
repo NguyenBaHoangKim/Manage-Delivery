c8dd553a862df153927849dbd4dfc4ceb2bee24f
NguyenBaHoangKim <baloctq@gmail.com>
2023-12-29 20:23:14 +0700

    k

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -30,21 +30,23 @@ export class AuthService {
   }
 
   getToken(): string | null {
-<<<<<<< HEAD
-    let token = localStorage.getItem('token');
+    let tokenString = localStorage.getItem('token');
+
+if (tokenString !== null && tokenString !== undefined) {
+  let token = JSON.parse(tokenString);
+  // Bây giờ bạn có thể sử dụng biến 'token' mà không gặp lỗi
+} else {
+  console.error('Giá trị token từ localStorage là null hoặc undefined.');
+}
     // let token: string = "eyJhbGciOiJIUzM4NCJ9.eyJ1c2VyIjp7ImlkIjoiM2QzMWYyZDctYTU3ZS0xMWVlLWE2NDUtM2JlZTFmM2M5M2JlIiwiY29kZWlkIjoiUUwwNiIsInNlcnZpY2VBZGRyZXNzaWQiOiJLSDAxIiwidXNlcm5hbWUiOiJLaWVuIiwiZW1haWwiOiJxbDA3QGdtYWlsLmNvbSIsInBob25lX251bWJlciI6IjEyMzQ1Njc4OTAifSwic3ViIjoicWwwN0BnbWFpbC5jb20iLCJpYXQiOjE3MDM4MzUwMTcsImV4cCI6MTcwMzkyMTQxN30.7PTelesKvVd4knxxU2FIB4piE3bk1Na6MuBpLka7J8c5CjeLTJUwdHPTtvT9QKLk"
-=======
-    // let token = localStorage.getItem('token');
-    let token: string = "eyJhbGciOiJIUzM4NCJ9.eyJ1c2VyIjp7ImlkIjoiNWVkNzU3Y2UtYTY0MS0xMWVlLWIzY2YtNmJkNzkzNWQ0YzI4IiwiY29kZWlkIjoiS0gwMiIsInNlcnZpY2VBZGRyZXNzaWQiOiJLSDAxIiwidXNlcm5hbWUiOiJLaWVuIiwiZW1haWwiOiJLaWVuMDIiLCJwaG9uZV9udW1iZXIiOiIxMjM0NTY3ODkwIn0sInN1YiI6IktpZW4wMiIsImlhdCI6MTcwMzg1MTAzNywiZXhwIjoxNzAzOTM3NDM3fQ.GOfnkhkGQ7OMxSpuhWdh5lgJsohs0B4PSMqDUaett56lxEXdR18DVF6EwVzYx4RA"
 
->>>>>>> newfixkien
 
-    if (token !== null) {
-      console.log(token)
+    if (tokenString !== null) {
+      console.log(tokenString)
       // token = JSON.parse(token);
     }
 
-    return token;
+    return tokenString;
   }
 
   clean(): void {
